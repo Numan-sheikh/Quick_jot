81b81092e849df2f88d4c4310b76abe0c10daff9
Numan sheikh <108549392+Numan-sheikh@users.noreply.github.com>
2025-05-09 06:18:57 +0530

    Added firebase_options.sample.dart

---
+++ lib/firebase_options.sample.dart
@@ -0,0 +1,16 @@
+// firebase_options.sample.dart
+// Rename this file to firebase_options.dart and run `flutterfire configure`
+// to regenerate the actual Firebase configuration.
+
+// import 'package:firebase_core/firebase_core.dart';
+
+// const FirebaseOptions firebaseOptions = FirebaseOptions(
+//   apiKey: 'YOUR_API_KEY',
+//   appId: 'YOUR_APP_ID',
+//   messagingSenderId: 'YOUR_SENDER_ID',
+//   projectId: 'YOUR_PROJECT_ID',
+//   authDomain: 'YOUR_PROJECT.firebaseapp.com',
+//   storageBucket: 'YOUR_PROJECT.appspot.com',
+//   measurementId: 'YOUR_MEASUREMENT_ID',
+// );
+
